dae11fcadf969dda58544bfd0d4735f18131c684
Octavian Sandu <soctavianstefan@gmail.com>
2024-05-14 18:27:41 +0300

    Revert "Update Baza de date (sunt petardat si m-am chinuit 2 ore pentru 2 linii de cod)"
    This reverts commit 3e372fc0e74ba9028d852169b050d1ad83102fcf.
    
    	modified:   Frontend/src/components/Navbar.js

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg  bg-secondary">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Aplicatie Student
-          </Link>
-          <Link className='btn btn-outline-light' to="/adauga">
-            Adaugare Student
-          </Link>
-        </div>
-      </nav>
+        <nav className="navbar navbar-expand-lg  bg-secondary">
+             <div className="container-fluid">
+                <a className="navbar-brand" href="#">Aplicatie Student</a>
+                 <Link className='btn btn-outline-light' to="/adauga">
+                    Adaugare Student
+                 </Link>
+             </div>
+         </nav>
     </div>
-  );
+  )
 }
