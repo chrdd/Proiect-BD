3c11957d9bc5f37dcd1f9999510ec482a226bf23
Octavian Sandu <soctavianstefan@gmail.com>
2024-05-14 18:34:48 +0300

    Sunt petardat

--- Backend/src/main/java/com/example/demo/student/StudentController.java
+++ Backend/src/main/java/com/example/demo/student/StudentController.java
@@ -26,7 +26,7 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
-    
+
 
 
     @PutMapping(path="/update/{studentId}")
@@ -39,11 +39,11 @@ public class StudentController {
     public ResponseEntity<Student> findStudentById(@PathVariable("studentId") Long studentId){
         Student student = studentService.findStudentById(studentId);
         return new ResponseEntity<>(student, HttpStatus.OK);
-    }return new ResponseEntity<>(HttpStatus.OK);
+    }
 
     @DeleteMapping(path="/delete/{studentId}")
     public ResponseEntity<?> deleteStudent(@PathVariable("studentId") Long Id){
         studentService.deleteStudent(Id);
-
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
